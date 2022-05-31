680e9a1b150afe319e19011198fad4834599178d
unknown <s222732@student.uek.krakow.pl>
2022-05-31 16:31:58 +0200

    2022-05-31 FK

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,22 @@ from flask import render_template
 @app.route('/')
 
 @app.route('/index/<name>')
-def index(name="<<Hello World>>"):
-    return render_template("index.html", text=name)
+def index():
+    return render_template('index.html.jinja')
 
 
+@app.route('/extract/<product_id>')
+def extract(product_id):
+    pass
 
+@app.route('/products')
+def products():
+    pass
+
+@app.route('/author')
+def author():
+    return render_template('author.html.jinja')
+
+@app.route('/product/<product_id>')
+def product(product_id):
+    pass
