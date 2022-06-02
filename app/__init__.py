54dc7ad0822a24c0cd946d3aa6b184e1cfb054f0
unknown <s222732@student.uek.krakow.pl>
2022-06-02 20:26:49 +0200

    2022-06-02 repair FK

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,4 @@ app = Flask(__name__)
 
 from app import routes
 
-app.run(debug=True, use_debugger=True, use_reloader=True)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+app.run(debug=True)
