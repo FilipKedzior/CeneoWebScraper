1dc28f419677706c88603e71874476a8052fec8a
unknown <s222732@student.uek.krakow.pl>
2022-06-14 00:40:03 +0200

    homwork 13.06.2022 FK

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,7 +1,7 @@
 from app.parameters import selectors
 from app.utils import get_item
 
-class Opinion:
+class Opinion():
     def __init__(self, author="", recommendation=None, stars=0, content="", useful=0, useless=0, publish_date=None, purchase_date=None, pros=[], cons=[], opinion_id=""):
         self.author = author
         self.recommendation = recommendation
@@ -15,9 +15,11 @@ class Opinion:
         self.cons = cons
         self.opinion_id = opinion_id
         return self
-
+    
     def extract_opinion(self, opinion):
         for key, value in selectors.items():
             setattr(self, key, get_item(opinion, *value))
-            self.opinion_id = opinion["data-entry-id"]             
+        self.opinion_id = opinion["data-entry-id"]
         return self
+
+    
--- app/models/product.py
+++ app/models/product.py
@@ -1,16 +1,15 @@
-from app import app
+import os
+import json
+import requests
+import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup
 from app.utils import get_item
 from app.models.opinion import Opinion
-import requests
-import os
-import pandas as pd
-import numpy as np
 from matplotlib import pyplot as plt
-import json
 
-class Product:
-    def __init__(self, product_id=0, opinions=[], product_name="", opinions_count=0, pros_count=0, cons_count=0, average_score=0):
+class Product():
+    def __init__(self, product_id, opinions=[], product_name="", opinions_count=0, pros_count=0, cons_count=0, average_score=0):
         self.product_id = product_id
         self.product_name = product_name
         self.opinions = opinions
@@ -19,18 +18,16 @@ class Product:
         self.cons_count = cons_count
         self.average_score = average_score
         return self
-    
+
     def extract_name(self):
         url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
         response = requests.get(url)
         page = BeautifulSoup(response.text, "html.parser")
         self.product_name = get_item(page,"h1.product-top__product-info__name")
-        
         return self
 
-    def extract_opinions(self, opinion):
+    def extract_opinions(self):
         url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
-        all_opinions = []
         while(url):
             response = requests.get(url)
             page = BeautifulSoup(response.text, "html.parser")
@@ -39,15 +36,16 @@ class Product:
                 single_opinion = Opinion().extract_opinion(opinion)
                 self.opinions.append(single_opinion)
             try:
-                url = "https://www.ceneo.pl"+get_item(page, "a.pagination__next", "href")
+                url = "https://www.ceneo.pl"+get_item(page,"a.pagination__next","href")
             except TypeError:
                 url = None
         return self
     
+    def opinions_to_df(self):
+        return pd.read_json(json.dumps([opinion.to_dict() for opinion in self.opinions]))
+
     def calculate_stats(self):
-        
-        opinions = pd.read_json(json.dumps([opinion.to_dict() for opinion in self.opinions]))
-        
+        opinions = self.opinions_to_df()
         opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
         
         self.opinions_count = len(opinions)
@@ -57,12 +55,8 @@ class Product:
         
         return self
     
-    def opinions_to_df(self):
-        opinions = self.opinions_to_df()
-        opinions["stars"] = opinions["stars"]
-        
     def draw_charts(self):
-        
+        opinions = self.opinions_to_df()
         if not os.path.exists("app/plots"):
             os.makedirs("app/plots")
         recommendation = opinions["recommendation"].value_counts(dropna=False).sort_index().reindex(["Nie polecam", "Polecam", None], fill_value=0)
@@ -73,7 +67,7 @@ class Product:
             labels = ["Nie polecam", "Polecam", "Nie mam zdania"]
         )
         plt.title("Rekomendacje")
-        plt.savefig(f"app/plots/{self.product_id}_recommendations.png")
+        plt.savefig(f"app/static/plots/{self.product_id}_recommendations.png")
         plt.close()
         stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
         stars.plot.bar(
@@ -84,25 +78,43 @@ class Product:
         plt.ylabel("Liczba opinii")
         plt.grid(True, axis="y")
         plt.xticks(rotation=0)
-        plt.savefig(f"app/plots/{self.product_id}_stars.png")
+        plt.savefig(f"app/static/plots/{self.product_id}_stars.png")
         plt.close()
         return self
-    
-    def __str__(self) -> str:
-        pass
-    
+
+    def __str__(self, product_id, opinions, product_name, opinions_count, pros_count, cons_count, average_score) -> str:
+        self.product_id = product_id
+        self.product_name = product_name
+        self.opinions = opinions
+        self.opinions_count = opinions_count
+        self.pros_count = pros_count
+        self.cons_count = cons_count
+        self.average_score = average_score
+        return self
+        
+
     def __repr__(self) -> str:
         pass
-    
+
     def to_dict(self) -> dict:
+        # >>> class A(object):
+        # ...   def __init__(self):
+        # ...     self.b = 1
+        # ...     self.c = 2
+        # ...   def do_nothing(self):
+        # ...     pass
+        # ...
+        # >>> a = A()
+        # >>> a.__dict__
+        # {'c': 2, 'b': 1}
         pass
-    
+
     def export_opinions(self):
         if not os.path.exists("app/opinions"):
             os.makedirs("app/opinions")
         with open(f"app/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
-            json.dump(opinion.to_dict(), jf, indent=4, ensure_ascii=False)
-        return redirect(url_for('product', product_id=self.product_id))
-    
-    def export_product(self):
+            json.dump([opinion.to_dict() for opinion in self.opinions], jf, indent=4, ensure_ascii=False)
+        pass
+
+    def export_product(swelf):
         pass
